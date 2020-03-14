f07270507cdc9e5166a87cb76f1b3b16602e7ac4
工藤海斗 <kaitokudou2468110921@gmail.com>
2020-03-14 15:57:36 +0900

    とりあえず、カスタムセルのオートレイアウトをつけた

--- SummerVacationHomework/SummerVacationHomework/API/ResponseWether.swift
+++ SummerVacationHomework/SummerVacationHomework/API/ResponseWether.swift
@@ -15,17 +15,6 @@ struct Forecasts: Codable{
     
     var temperature: Temperature // 気温
     var image: Image // 天気の画像
-    
-    /*init(dataLabel:String, telop:String, data:String, temperature:Temperature, image:Image) {
-        self.dateLabel = dataLabel
-        self.telop = telop
-        self.date = data
-        self.temperature = temperature
-        self.image = image
-        
-    }*/
-    
-    
 }
 
 struct Temperature: Codable{
@@ -36,46 +25,22 @@ struct Temperature: Codable{
 struct Image: Codable{
     var url: URL
     var title: String
-    
-    /*init(url:URL, title:String) {
-        self.url = url
-        self.title = title
-    }*/
 }
 
 struct Max: Codable {
     var celsius: String // 最高気温
-    
-    /*init(celsius:String) {
-        self.celsius = celsius
-    }*/
 }
 
 struct Min: Codable {
     var celsius: String // 最低気温
-    
-    /*init(celsius:String) {
-        self.celsius = celsius
-    }*/
 }
 
 struct Location: Codable {
     var city: String // 市町村
     var prefecture: String // 都道府県
-    
-    /*init(city:String, prefecture:String) {
-        self.city = city
-        self.prefecture = prefecture
-    }*/
 }
 
 struct WetherData: Codable {
     var forecasts: [Forecasts]
     var location: Location
-    
-    /*init(forecasts:[Forecasts], location:Location)
-    {
-        self.forecasts = forecasts
-        self.location = location
-    }*/
 }
